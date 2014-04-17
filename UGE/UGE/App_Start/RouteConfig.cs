456b121ec43c547162a80a3f2a0133eb56c168b1
Alim Ul Karim <auk.junk@live.com>
2014-04-18 01:59:20 +0600

    Modified1

--- UGE/UGE/App_Start/RouteConfig.cs
+++ UGE/UGE/App_Start/RouteConfig.cs
@@ -13,7 +13,10 @@ namespace UGE {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: 
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                 , namespaces: new[] { "UGE.Controllers" }
+
             );
         }
     }
