32d68da4d7194fa31748431beccd1b71c020bccf
Alim Ul Karim <auk.junk@live.com>
2014-04-18 01:25:18 +0600

    Project Created
    Project Created

---
+++ UGE/UGE/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace UGE {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ UGE/UGE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UGE.Startup))]
+namespace UGE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
